df5c7960a470a70d5cc5999a83d13ebc08934e38
carlosblancomarketing <carlosblancosierra@gmail.com>
2020-06-09 14:08:27 +0200

    feat: get data from api and implement Book and Bookshelf components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from '../BooksAPI'
 import './App.css'
 import Dashboard from './Dashboard';
 import SearchBooks from './SearchBooks';
@@ -7,31 +7,31 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
 
   handleCloseSearch = () => {
     this.setState({ showSearchPage: false });
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
   render() {
     return (
       <Router>
-        
+
         <Route path="/" exact render={() => (
-          <Dashboard />
-        )}/>
+          <Dashboard books={this.state.books}/>
+        )} />
 
         <Route path="/search" render={() => (
           <SearchBooks />
-        )}/>
-        
+        )} />
+
       </Router>
     )
   }
