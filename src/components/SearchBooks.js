21933cb866f76fba3bb5f8606411ccd407725767
carlosblancomarketing <carlosblancosierra@gmail.com>
2020-06-10 14:54:51 +0200

    fix: searchbooks api call

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -7,24 +7,8 @@ class SearchBooks extends Component {
         books: [],
         query: "",
     }
-    closeSearch = () => {
-        this.props.handleCloseSearch();
-    }
 
-    handleUpdateQuery = (e) => {
-        
-        this.state.timeout = setTimeout(this.updateQuery(e), 1000)
-    }
-
-    updateQuery = (e) => {
-        let timeout;
-        const query = e.target.value.trim();
-        clearTimeout(this.state.timeout);
-
-        if (query === "") {
-            this.setState({query, books:[]})
-        }
-
+    getBooksFromApi = (query) => {
         this.props.BooksAPI.search(query).then((books) => {
 
             let showBooks = true
@@ -44,6 +28,17 @@ class SearchBooks extends Component {
         })
     }
 
+    updateQuery = (e) => {
+        const query = e.target.value.trim();
+
+        if (query === "") {
+            this.setState({ query, books: [] })
+        }
+
+        this.getBooksFromApi(query)
+
+    }
+
     render() {
 
         return (
@@ -52,18 +47,10 @@ class SearchBooks extends Component {
                     <Link className="close-search" to="/" />
 
                     <div className="search-books-input-wrapper">
-                        {/*
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-                        <input
+                          <input
                             type="text"
                             placeholder="Search by title or author"
-                            onChange={this.handleUpdateQuery}
+                            onChange={this.updateQuery}
                         />
 
                     </div>
